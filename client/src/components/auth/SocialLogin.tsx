b28b5d95b32ebc4bacfba266389d449a31bd210d
Devid2207 <devid220701@gmail.com>
2021-11-15 21:47:15 +0700

    Blog Web

---
+++ client/src/components/auth/SocialLogin.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { GoogleLogin, GoogleLoginResponse } from 'react-google-login-lite';
+
+import { googleLogin } from '../../redux/actions/authAction'
+
+const SocialLogin = () => {
+  const dispatch = useDispatch()
+
+  const onSuccess = (googleUser: GoogleLoginResponse) => {
+    const id_token = googleUser.getAuthResponse().id_token
+    dispatch(googleLogin(id_token))
+  }
+  
+  
+
+  return (
+    <div className="my-2">
+      <GoogleLogin 
+      client_id='987377843939-9cm0aqahk1gidfae0h22ctoiolcl4sva.apps.googleusercontent.com'
+      cookiepolicy='single_host_origin'
+      onSuccess={onSuccess}
+      />
+    </div>
+  )
+}
+
+export default SocialLogin
